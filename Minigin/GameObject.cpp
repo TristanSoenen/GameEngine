bb0afd79170f57c0b90e926994b28021e4096122
TristanSoenen <soenentristan@gmail.com>
2024-02-26 09:52:44 +0100

    added add componennt remove and has been added

--- Minigin/GameObject.cpp
+++ Minigin/GameObject.cpp
@@ -2,17 +2,37 @@
 #include "GameObject.h"
 #include "ResourceManager.h"
 #include "Renderer.h"
+#include "Component.h"
+#include "algorithm"
+
 
 dae::GameObject::~GameObject() = default;
 
-void dae::GameObject::Fixed_Update(){}
+void dae::GameObject::Fixed_Update()
+{
+	for (auto& component : m_Components)
+	{
+		component->Fixed_Update();
+	}
+}
 
-void dae::GameObject::Update(){}
+void dae::GameObject::Update()
+{
+	for (auto& component : m_Components)
+	{
+		component->Update();
+	}
+}
 
 void dae::GameObject::Render() const
 {
 	const auto& pos = m_transform.GetPosition();
 	Renderer::GetInstance().RenderTexture(*m_texture, pos.x, pos.y);
+
+	for (auto& component : m_Components)
+	{
+		component->Render();
+	}
 }
 
 void dae::GameObject::SetTexture(const std::string& filename)
@@ -23,4 +43,28 @@ void dae::GameObject::SetTexture(const std::string& filename)
 void dae::GameObject::SetPosition(float x, float y)
 {
 	m_transform.SetPosition(x, y, 0.0f);
+}
+
+void dae::GameObject::AddComponent(std::unique_ptr<Component> component)
+{
+	m_Components.emplace_back(std::move(component));
+}
+
+void dae::GameObject::RemoveComponent(std::unique_ptr<Component> component)
+{
+	m_Components.erase(std::remove(m_Components.begin(), m_Components.end(), component));
+}
+
+//dae::Component* dae::GameObject::GetComponent()
+//{
+//	//return 
+//}
+
+bool dae::GameObject::HasComponentBeenAdded(std::unique_ptr<Component> component)
+{
+	if (std::find(m_Components.begin(), m_Components.end(), component) != m_Components.end())
+	{
+		return true;
+	}
+	return false;
 }
--- Minigin/GameObject.h
+++ Minigin/GameObject.h
@@ -1,18 +1,30 @@
 #pragma once
 #include <memory>
+#include "Component.h"
 #include "Transform.h"
+#include "vector"
 
 namespace dae
 {
 	class Texture2D;
+	//class Component;
 
+	
 	// todo: this should become final.
 	class GameObject
 	{
 	public:
+		
 		virtual void Fixed_Update();
 		virtual void Update();
 		virtual void Render() const;
+
+		void AddComponent(std::unique_ptr<Component> component);
+		void RemoveComponent(std::unique_ptr<Component> component);
+		//Component* GetComponent();
+		
+		bool HasComponentBeenAdded(std::unique_ptr<Component> component);
+
 
 		void SetTexture(const std::string& filename);
 		void SetPosition(float x, float y);
@@ -25,6 +37,7 @@ namespace dae
 		GameObject& operator=(GameObject&& other) = delete;
 
 	private:
+		std::vector<std::unique_ptr<Component>> m_Components;
 		Transform m_transform{};
 		// todo: mmm, every gameobject has a texture? Is that correct?
 		std::shared_ptr<Texture2D> m_texture{};
