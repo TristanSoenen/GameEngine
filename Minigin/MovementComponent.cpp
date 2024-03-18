adffddce3d80f230a89a1ad430eb5738cc31f139
TristanSoenen <soenentristan@gmail.com>
2024-03-18 10:44:42 +0100

    last changes before hand in

--- Minigin/MovementComponent.cpp
+++ Minigin/MovementComponent.cpp
@@ -6,30 +6,31 @@
 
 dae::MovementComponent::MovementComponent(GameObject* pOwner)
 	:Component{ pOwner }
+	,m_MovementSpeed{100.0f}
 {
 	m_TransformComp = pOwner->GetComponent<dae::TransformComponent>();
 	assert(m_TransformComp);
-	m_MovementSpeed = 50.0f;
 }
 
 void dae::MovementComponent::MovementInput(Direction direction)
 {
+	float deltaTime = DeltaTime::GetInstance().GetDeltaTime();
 	m_direction = direction;
 	auto currentPosition = m_TransformComp->GetPosition();
 	glm::vec3 nextPos = currentPosition;
 	switch (m_direction)
 	{
 	case dae::Direction::Up:
-		nextPos.y -= m_MovementSpeed * DeltaTime::GetInstance().GetDeltaTime();
+		nextPos.y -= m_MovementSpeed * deltaTime;
 		break;
 	case dae::Direction::Down:
-		nextPos.y += m_MovementSpeed * DeltaTime::GetInstance().GetDeltaTime();
+		nextPos.y += m_MovementSpeed * deltaTime;
 		break;
 	case dae::Direction::Left:
-		nextPos.x -= m_MovementSpeed * DeltaTime::GetInstance().GetDeltaTime();
+		nextPos.x -= m_MovementSpeed * deltaTime;
 		break;
 	case dae::Direction::Right:
-		nextPos.x += m_MovementSpeed * DeltaTime::GetInstance().GetDeltaTime();
+		nextPos.x += m_MovementSpeed * deltaTime;
 		break;
 	case dae::Direction::none:
 		break;
