f41bbf29749aa6b0875b584ac42a8e66279f6ead
TristanSoenen <soenentristan@gmail.com>
2024-03-22 20:06:22 +0100

    added a keyboard class to procces keyboard input.

---
+++ Minigin/Keyboard.cpp
@@ -0,0 +1,17 @@
+#include "Keyboard.h"
+
+bool dae::Keyboard::ProcessInput()
+{
+	if (!m_KeysAndCommands.empty())
+	{
+		for (int index{ 0 }; index < int(m_KeysAndCommands.size()); index++)
+		{
+			if (IsPressed(m_KeysAndCommands[index]))
+			{
+				m_KeysAndCommands[index];
+			}
+		}
+	}
+
+	return true;
+}
---
+++ Minigin/Keyboard.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <SDL.h>
+#include "Command.h"
+#include <vector>
+#include <memory>
+
+namespace dae
+{
+
+	class Keyboard
+	{
+	public:
+		Keyboard() = default;
+		~Keyboard() = default;
+
+		bool ProcessInput();
+		bool IsPressed(unsigned int key) { const Uint8* state = SDL_GetKeyboardState(NULL);  return state[key]; }
+
+	private:
+
+		//vector of keys and vector of commands use map instead?
+		std::vector<int, std::unique_ptr<Command>> m_KeysAndCommands;
+
+	};
+}
+
+
