5d9a0e63280831c162826f02ba49ce732870e211
TristanSoenen <soenentristan@gmail.com>
2024-04-05 20:01:31 +0200

    removed thrash the cashe and added filters

--- Minigin/Renderer.cpp
+++ Minigin/Renderer.cpp
@@ -2,7 +2,6 @@
 #include "Renderer.h"
 #include "SceneManager.h"
 #include "Texture2D.h"
-#include "TrashTheCashe.h"
 #include "backends/imgui_impl_opengl3.h"
 #include "backends/imgui_impl_sdl2.h"
 #include "imgui_plot.h"
