cc9a08be921c1ac583cd0db10bae5b01857acf3d
TristanSoenen <soenentristan@gmail.com>
2024-04-15 19:18:25 +0200

    no result yet of the text component close tho

--- Minigin/TextComponent.cpp
+++ Minigin/TextComponent.cpp
@@ -1,8 +1,30 @@
 #include "TextComponent.h"
 #include "GameObject.h"
+#include "TextObject.h"
+#include "TransformComponent.h"
+#include "Font.h"
 
-dae::TextComponent::TextComponent(dae::GameObject* pOwner)
+dae::TextComponent::TextComponent(dae::GameObject* pOwner, std::string text, std::shared_ptr<dae::Font> font)
 	:Component(pOwner)
 {
+	m_TextObject = std::make_unique<TextObject>(text, font);
 
+	auto transformComp =  pOwner->GetComponent<TransformComponent>();
+	auto pos = transformComp->GetPosition();
+	m_TextObject->SetPosition(pos.x, pos.y);
+}
+
+void dae::TextComponent::Render()
+{
+	m_TextObject->Render();
+}
+
+void dae::TextComponent::SetPosition(float x, float y)
+{
+	m_TextObject->SetPosition(x, y);
+}
+
+void dae::TextComponent::SetText(std::string text)
+{
+	m_TextObject->SetText(text);
 }
--- Minigin/TextComponent.h
+++ Minigin/TextComponent.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "Component.h"
+#include <memory>
 namespace dae
 {
+	class Font;
+	class TextObject;
 	class GameObject;
 
 	class TextComponent : public Component
 	{
 	public:
-		TextComponent(GameObject* pOwner);
+		TextComponent(GameObject* pOwner, std::string text, std::shared_ptr<Font> font);
 		~TextComponent() = default;
+		void Render() override;
+		void SetPosition(float x, float y);
+		void SetText(std::string text);
 	private:
-
+		std::unique_ptr<TextObject> m_TextObject;
 	};
 }
